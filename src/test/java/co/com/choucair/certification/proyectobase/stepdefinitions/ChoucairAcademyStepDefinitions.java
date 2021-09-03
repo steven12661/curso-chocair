38ed938a68e30d0c61d4e4047bd9ae0d8eddd816
Steven Sanchez <steven12661@gmail.com>
2021-09-02 22:21:20 -0500

    initial commit

---
+++ src/test/java/co/com/choucair/certification/proyectobase/stepdefinitions/ChoucairAcademyStepDefinitions.java
@@ -0,0 +1,28 @@
+package co.com.choucair.certification.proyectobase.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+
+import java.util.List;
+
+public class ChoucairAcademyStepDefinitions {
+
+    @Given("^than Jeff wants to learn automation course at the Choucair Academy$")
+    public void thanJeffWantsToLearnAutomationCourseAtTheChoucairAcademy() {
+
+    }
+
+
+    @When("^he searches for the course Recursos Automatización Bancolombia on the Choucair Academy platform$")
+    public void heSearchesForTheCourseRecursosAutomatizaciónBancolombiaOnTheChoucairAcademyPlatform() {
+
+    }
+
+    @Then("^he finds the course called resources Recursos Automatización Bancolombia$")
+    public void heFindsTheCourseCalledResourcesRecursosAutomatizaciónBancolombia() {
+
+    }
+
+}
---
+++ src/test/resources/features/academyChoucair.feature
@@ -0,0 +1,9 @@
+# Author: Steven Sanchez Restrepo
+@stories
+Feature: Academy Choucair
+  As a user, I want to learn how to automate in screenplay at the Choucair Academy with the automation course
+  @scenario1
+  Scenario: Search for a automation course
+  Given than Jeff wants to learn automation course at the Choucair Academy
+  When he searches for the course Recursos Automatización Bancolombia on the Choucair Academy platform
+  Then he finds the course called resources Recursos Automatización Bancolombia
